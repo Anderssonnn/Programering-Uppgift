dfb68cecc4c0f81412de5aad6fd1a80f164e2c29
Anderssonnn <69970038+Anderssonnn@users.noreply.github.com>
2020-11-04 15:00:30 +0100

    Skapade en ny kod
    Console readline vad du heter och console write line som skriver ut ditt namn med 2 st strings

---
+++ text1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du?");
+            string text1 = Console.ReadLine();
+            string text2 = "testar";
+            string text3 = "Github";
+            Console.WriteLine(text1 +" "+ text2 + " " + text3);
+        }
+    }
+}
